3320bb4ae66b35617da3343d5a416e7c52253568
Fadhlan Ridhwanallah <fridhwanallah@gmail.com>
2025-08-21 10:31:28 +0700

    bump x0 sdk 1.0.10

--- src/App.tsx
+++ src/App.tsx
@@ -26,6 +26,13 @@ function App() {
             decimals: 6,
             hypColAddress: '0x2DA752397487D7C44bf94c3cA433a4257ba87773',
           },
+          {
+            symbol: 'USDC',
+            name: 'USDC',
+            address: '0x3c3Cac8e7258C5928a89e1dFe804b7D7cab6429e',
+            decimals: 6,
+            hypColAddress: '0x3c3Cac8e7258C5928a89e1dFe804b7D7cab6429e',
+          },
         ],
         hookAddress: '0x14fD8C5eAe7A7F9AB17b605Dc7b12aC24A9329A6',
         destinationDomain: 137,
@@ -54,7 +61,7 @@ function App() {
           onError={(err) => alert(`Error: ${err.message}`)}
           enableWalletConnect={true}
           walletConnectProjectId="ae3afbf40e1ac9b7cf433248048853c1"
-          workerApiUrl="http://relayer.x0pay.com"
+          workerApiUrl="https://relayer.x0pay.com"
         />
       </main>
     </div>
